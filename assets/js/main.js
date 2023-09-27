bef689e56a877511a4a63497e66f05d514624383
bandiiskola <nandor.bere@gmail.com>
2023-09-27 11:04:54 +0200

    Adatok

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+var csokik = [{
+        nev: "Milka",
+        kep: "Milka.png",
+        kiszereles: "100g",
+        orszag: "Német"
+    },
+    {
+        nev: "Feastables",
+        kep: "Feastables.png",
+        kiszereles: "60g",
+        orszag: "USA"
+    },
+    {
+        nev: "Hersheys",
+        kep: "Hersheys.png",
+        kiszereles: "150g",
+        orszag: "USA"
+    },
+    {
+        nev: "Tibi",
+        kep: "Tibi.png",
+        kiszereles: "100g",
+        orszag: "Magyar"
+    },
+    {
+        nev: "Snickers",
+        kep: "Snickers.png",
+        kiszereles: "45g",
+        orszag: "USA"
+    }
+]
